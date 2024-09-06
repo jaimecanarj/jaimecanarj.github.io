f9815ec357bba7a6d5ac75f423fa8f465cd26407
Jaime <jaimecanarj@gmail.com>
2024-09-06 14:40:22 +0200

    Añadida librería shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Button>Primer botón</Button>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
