9578642353236abb877f5d44c6c9ac0c4844a8b9
Jaime <jaimecanarj@gmail.com>
2024-09-07 09:32:08 +0200

    Añadida sección proyectos

---
+++ src/components/Projects.tsx
@@ -0,0 +1,61 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const projects = [
+  {
+    title: "Título 1",
+    image: "/project.svg",
+    href: "#",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magnam vitae qui animi reiciendis fugit illum libero ab ipsum aspernatur!",
+  },
+  {
+    title: "Título 2",
+    image: "/project.svg",
+    href: "#",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dolorem quidem vero voluptatum enim! Corrupti rem similique asperiores incidunt quas?",
+  },
+];
+
+const Projects = () => {
+  return (
+    <section
+      id="proyectos"
+      className="flex flex-col items-center justify-center min-h-screen"
+    >
+      <h1 className="pb-4 m-16 text-6xl font-semibold border-b-4 border-current">
+        Proyectos
+      </h1>
+      <div className="container grid gap-8 p-8 lg:grid-cols-2">
+        {projects.map((project) => {
+          return (
+            <a href={project.href} key={project.href}>
+              <Card className=" sm:h-60 hover:bg-secondary">
+                <CardHeader className="flex flex-col items-center">
+                  <CardTitle className="mb-2">{project.title}</CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col items-center sm:flex-row gap-x-4">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    width={160}
+                    className="m6"
+                  />
+                  <p>{project.description}</p>
+                </CardContent>
+              </Card>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
