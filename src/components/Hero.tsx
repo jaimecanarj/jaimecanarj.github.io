1a06607a866d393c41691eb11550218fdfc1cbc6
Jaime <jaimecanarj@gmail.com>
2024-09-07 09:09:22 +0200

    Arreglada visibilidad de Hero con diferentes temas

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
     <section className="flex flex-col items-center justify-center min-h-screen hero">
-      <h1 className="m-8 text-4xl sm:text-6xl">Hola, soy Jaime</h1>
-      <p className="max-w-sm sm:max-w-xl text-md sm:text-lg">
+      <h1 className="m-8 text-4xl text-white sm:text-6xl">Hola, soy Jaime</h1>
+      <p className="max-w-sm text-white sm:max-w-xl text-md sm:text-lg">
         Soy un estudiante de desarrollo de aplicaciones web apasionado por la
         creación de soluciones digitales innovadoras. Me especializo en
         tecnologías como HTML, CSS y JavaScript, y estoy aprendiendo frameworks
